a5c353d12d538482fd4d3df536ad6f9f34d7f7a9
Andrew <dirai2111@gmail.com>
2019-02-07 22:47:16 +0300

    add sides, add bicycle components

---
+++ src/components/Side/Side.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './side.css'
+
+class Side extends Component {
+    render() {
+        return (
+            <div className="side">
+                <header className="sideHeader">
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Side
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Bicycle from './Bicycle/Bicycle'
+import Side from './Side/Side'
+
+export {
+    Bicycle,
+    Side,
+}
